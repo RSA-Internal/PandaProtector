c4adc3c56faf80f65726e3b89429f0ff6f664e1f
TSP_Panda <chris.barwick16@gmail.com>
2021-02-15 17:45:47 -0800

    Initial Setup + Request Command

---
+++ commands/request.js
@@ -0,0 +1,9 @@
+const { requestChannel } = require('../config.json');
+
+module.exports = {
+    name: 'request',
+    description: 'Request any online staff members to the current channel',
+    execute(message, args) {
+        
+    },
+};
