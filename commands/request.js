807ec087087b6f5f890f2dad4d1ca50914551d89
TSP_Panda <chris.barwick16@gmail.com>
2021-02-16 11:01:10 -0800

    Request Command

--- commands/request.js
+++ commands/request.js
@@ -1,9 +1,29 @@
-const { requestChannel } = require('../config.json');
+const { requestChannelId, staffId, pandabotId } = require('../config.json');
 
 module.exports = {
     name: 'request',
     description: 'Request any online staff members to the current channel',
+    cooldown: 30,
     execute(message, args) {
-        
+        const guild = message.guild;
+        const channelLocation = message.channel;
+        const channel = guild.channels.resolve(requestChannelId);
+        const reason = args.join(' ');
+        const author = message.author
+
+        if (reason.length <= 2) {
+            message.reply("Please provide a better reason to summon staff.");
+            return;
+        }
+
+        if (channel) {
+            let staffMembers = guild.roles.resolve(pandabotId).members.filter(member => member.presence.status == 'online' || member.presence.status == 'idle');
+            let staffMentions = "";
+
+            staffMembers.each(member => staffMentions = `${staffMentions} ${member},`);
+
+            channel.send(`${staffMentions}\n\n${author} has request a mod to ${channelLocation} with reason: ${reason}.`);
+            return;
+        }
     },
 };
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const { prefix, token, showcaseChannelId } = require('./config.json');
+
+const cooldowns = new Discord.Collection();
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -17,15 +19,55 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    //Showcase check
+    const content = message.content;
+    const channel = message.channel;
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
+    const deleted = false;
 
-    if (!client.commands.has(command)) return;
+    if (channel.id == showcaseChannelId) {
+        if (message.attachments.length == 0) {
+            if (!content.includes(".com") || !content.includes(".net") || !content.includes("prnt.sc")) {
+                message.delete();
+                deleted = true;
+            }
+        }
+    }
+
+    if (deleted) return;
+
+    //Command check
+    if (!content.startsWith(prefix) || message.author.bot) return;
+
+    const args = content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    if (!client.commands.has(commandName)) return;
+
+    const command = client.commands.get(commandName);
+
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Discord.Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+
+    if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+            return message.reply(`Please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+        }
+    }
+
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
     try {
-        client.commands.get(command).execute(message, args);
+        command.execute(message, args);
     } catch (error) {
         console.error(error);
         message.reply('There was an error trying to execute that command!');
