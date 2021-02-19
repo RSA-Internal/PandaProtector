22fc468aa4df856b2adca7f93c2e9ea4d5686cf1
TSP_Panda <chris.barwick16@gmail.com>
2021-02-18 22:20:59 -0800

    Convert lua.js to WandboxAPI

--- commands/fun/lua.js
+++ commands/fun/lua.js
@@ -1,48 +1,54 @@
 const Discord = require('discord.js');
-const glua = require('glua');
+var runWandbox = require('wandbox-api-updated');
 
 module.exports = {
     name: 'lua',
     description: 'Compiles provided Lua, and provides output.',
-    cooldown: 60,
     args: true,
-    execute(message, args) {
+    async execute(message, args) {
         var src = args.join(' ');
+
         var output = [];
         var errors = [];
 
-        try {
-            glua.runWithGlobals({
-                print: function () {
-                    for (v of arguments) {
-                        output.push(v);
-                    }
-                },
-                io: `nil`,
-                os: `nil`,
-                debug: `nil`
-                //Can leave room for future implementation of other classes such as Vector2, Vector3, etc
-            }, src);
-        } catch ( error ) {
-            errors.push(error);
-        }
+        await runWandbox.fromString(src, {'compiler': 'lua-5.3.0'}, function clbk( error, results) {
+            if (results.program_error) {
+                errors.push(results.program_error);
+            }
 
-        const resultEmbed = new Discord.MessageEmbed()
-            .setColor('#0cc218')
-            .setTitle('Result of Code')
-            .setAuthor(`${message.guild.members.resolve(message.author.id).displayName}`)
-            .setDescription('Lua Code')
-            .addFields(
-                { name: 'Source Preview', value: src.slice(0, 100)},
-                { name: 'Output', value: output.join('\n')},
-            )
-            .setTimestamp();
+            if (results.program_output) {
+                output.push(results.program_output);
+            }
 
-        if (errors.length) {
-            resultEmbed.setColor('#e32214');
-            resultEmbed.addField('Error(s)', errors.join('\n'));
-        }
+            if (error) {
+                errors.push(error);
+            }
 
-        message.channel.send(resultEmbed);
+            if (!src.length) {
+                src = "No source?";
+            }
+    
+            if (!output.length) {
+                output.push('No output.');
+            }
+    
+            const resultEmbed = new Discord.MessageEmbed()
+                .setColor('#0cc218')
+                .setTitle('Result of Code')
+                .setAuthor(`${message.guild.members.resolve(message.author.id).displayName}`)
+                .setDescription('Lua Code')
+                .addFields(
+                    { name: 'Source Preview', value: src.slice(0, 100)},
+                    { name: 'Output', value: output.join('\n').slice(0, 1024)},
+                )
+                .setTimestamp();
+    
+            if (errors.length) {
+                resultEmbed.setColor('#e32214');
+                resultEmbed.addField('Error(s)', errors.join('\n'));
+            }
+    
+            message.channel.send(resultEmbed);
+        });
     }
 }
