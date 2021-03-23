4f45a7734af77e6e2148bbaeb276bf3b9b8b4230
TSP_Panda <chris.barwick16@gmail.com>
2021-03-23 12:38:22 -0700

    Fixed Member Role not defined

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'], ws: ['GUILD_MEMBERS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS'] });
 const path = require('path');
 const helper = require('./util/helper');
-const { applicationID, publicKey, token, guildId, roleStaffId, testToken, channelRequestId, testPublicKey, testApplicationID, testGuildId, testChannelRequestId, testRoleStaffId, channelShowcaseId, channelRulesId, emojiYaysId } = require('./config.json');
+const { applicationID, publicKey, token, guildId, roleStaffId, testToken, channelRequestId, testPublicKey, testApplicationID, testGuildId, testChannelRequestId, testRoleStaffId, channelShowcaseId, channelRulesId, emojiYaysId, roleMemberId } = require('./config.json');
 
 var args = process.argv.slice(2);
 var testing = false;
