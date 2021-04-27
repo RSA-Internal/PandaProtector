d53b61f43dae5f223ee525452586475d5030ccf0
Chris <chris.barwick16@gmail.com>
2021-04-26 20:08:42 -0700

    Define list of langs for compile

--- src/commands/compile.ts
+++ src/commands/compile.ts
@@ -14,29 +14,58 @@ const command: Command = {
 			name: "save",
 			description: "Save the code or not",
 		},
-        {
-            name: "src",
-            description: "Source to compile",
-        },
+		{
+			name: "src",
+			description: "Source to compile",
+		},
 	],
 	hasPermission: () => true,
 	parseArguments: defaultArgumentParser,
 	handler: (state, message, lang, save, ...src) => {
 		fromStringV2(
-            {
-                compiler: "lua-5.4.0",
-                code: src.join(" "),
-                save: save == 'true'
-            },
-            function( err, res ) {
-                if ( err ) {
-                    message.reply(`Compilation failed.\n${err.message}`);
-                } else {
-                    message.reply(res);
-                }
-            },
-            undefined
-        );
+			{
+				compiler:
+					lang == "C"
+						? "gcc-head-c"
+						: lang == "C#"
+						? "mono-head"
+						: lang == "C++" || lang == "CPP"
+						? "gcc-head"
+						: lang == "Haskell"
+						? "ghc-head"
+						: lang == "Java"
+						? "openjdk-head"
+						: lang == "JavaScript" || lang == "js"
+						? "nodejs-head"
+						: lang == "lisp" || lang == "clisp"
+						? "clisp-2.49"
+						: lang == "lua"
+						? "lua-5.4.0"
+						: lang == "pascal"
+						? "fpc-head"
+						: lang == "python"
+						? "pypy-head"
+						: lang == "ruby"
+						? "ruby-head"
+						: lang == "rust"
+						? "rust-head"
+						: lang == "scala"
+						? "scala-2.13.x"
+						: lang == "TypeScript" || lang == "ts"
+						? "typescript-3.9.5"
+						: "lua-5.4.0",
+				code: src.join(" "),
+				save: save == "true",
+			},
+			function (err, res) {
+				if (err) {
+					message.reply(`Compilation failed.\n${err.message}`).catch(reason => console.error(reason));
+				} else {
+					message.reply(res).catch(reason => console.error(reason));
+				}
+			},
+			undefined
+		);
 	},
 };
 
