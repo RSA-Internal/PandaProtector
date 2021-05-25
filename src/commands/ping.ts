0563e3d4b24a3aba0fc2d022140724607c28c73b
Chris <chris.barwick16@gmail.com>
2021-05-25 12:38:39 -0700

    Fixed lint errors on ping.ts

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -7,12 +7,16 @@ const command: Command = {
 	hasPermission: () => true,
 	shouldBeEphemeral: (state, interaction) => interaction.channelID !== state.config.botChannelId,
 	handler: (state, interaction) => {
-		interaction.reply("Pinging...", { ephemeral: command.shouldBeEphemeral(state, interaction) });
-		interaction.editReply(
-			`Websocket heartbeat: ${state.client.ws.ping}ms\nRoundtrip latency: ${
-				interaction.createdTimestamp - Date.now()
-			}ms`
-		);
+		interaction
+			.reply("Pinging...", { ephemeral: command.shouldBeEphemeral(state, interaction) })
+			.catch(console.error.bind(console));
+		interaction
+			.editReply(
+				`Websocket heartbeat: ${state.client.ws.ping}ms\nRoundtrip latency: ${
+					interaction.createdTimestamp - Date.now()
+				}ms`
+			)
+			.catch(console.error.bind(console));
 	},
 };
 
