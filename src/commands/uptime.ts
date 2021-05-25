9b4b119152027f3d9ea93658857c17d8ccdfb0e7
M39a9am3R <m39a9am3r@gmail.com>
2021-05-25 18:36:59 -0400

    Uptime formatting fix, resolves #59.

--- src/commands/uptime.ts
+++ src/commands/uptime.ts
@@ -7,8 +7,16 @@ const command: Command = {
 	hasPermission: () => true,
 	shouldBeEphemeral: (state, interaction) => interaction.channelID !== state.config.botChannelId,
 	handler: (state, interaction) => {
+		let uptimeInSeconds = process.uptime();
+		const uptimeDays = Math.floor(uptimeInSeconds / 86400);
+		uptimeInSeconds -= uptimeDays * 86400;
+		const uptimeHours = Math.floor(uptimeInSeconds / 3600);
+		uptimeInSeconds -= uptimeHours * 3600;
+		const uptimeMinutes = Math.floor(uptimeInSeconds / 60);
+		const uptimeSeconds = Math.floor(uptimeInSeconds - uptimeMinutes * 60);
+		//Output appearance: 1d:12h:30m:30s
 		interaction
-			.reply(`Uptime: ${Math.floor(process.uptime())} seconds.`, {
+			.reply(`Uptime: ${uptimeDays}d:${uptimeHours}h:${uptimeMinutes}m:${uptimeSeconds}s.`, {
 				ephemeral: command.shouldBeEphemeral(state, interaction),
 			})
 			.catch(console.error.bind(console));
