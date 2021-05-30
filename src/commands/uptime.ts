de42d42645f79c92353b58ec89b2b8c11600868e
Matthew <willyoumeryme@gmail.com>
2021-05-30 16:29:27 -0700

    Clean up

--- src/commands/uptime.ts
+++ src/commands/uptime.ts
@@ -7,16 +7,14 @@ const command: Command = {
 	hasPermission: () => true,
 	shouldBeEphemeral: (state, interaction) => interaction.channelID !== state.config.botChannelId,
 	handler: (state, interaction) => {
-		let uptimeInSeconds = process.uptime();
-		const uptimeDays = Math.floor(uptimeInSeconds / 86400);
-		uptimeInSeconds -= uptimeDays * 86400;
-		const uptimeHours = Math.floor(uptimeInSeconds / 3600);
-		uptimeInSeconds -= uptimeHours * 3600;
-		const uptimeMinutes = Math.floor(uptimeInSeconds / 60);
-		const uptimeSeconds = Math.floor(uptimeInSeconds - uptimeMinutes * 60);
-		//Output appearance: 1d:12h:30m:30s
+		const uptime = process.uptime();
+		const seconds = (Math.floor(uptime) % 60).toString().padStart(2, "0");
+		const minutes = (Math.floor(uptime / 60) % 60).toString().padStart(2, "0");
+		const hours = (Math.floor(uptime / 3600) % 60).toString().padStart(2, "0");
+		const days = Math.floor(uptime / 86400);
+
 		interaction
-			.reply(`Uptime: ${uptimeDays}d:${uptimeHours}h:${uptimeMinutes}m:${uptimeSeconds}s.`, {
+			.reply(`Uptime: ${days} days ${hours}:${minutes}:${seconds}`, {
 				ephemeral: command.shouldBeEphemeral(state, interaction),
 			})
 			.catch(console.error.bind(console));
