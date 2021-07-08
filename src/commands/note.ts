f8366a0e47b15196a41327ef74c6bc0c849326ab
M39a9am3R <m39a9am3r@gmail.com>
2021-07-08 14:21:12 -0400

    Finished /note command.

--- src/commands/note.ts
+++ src/commands/note.ts
@@ -1,6 +1,10 @@
-import type { TextChannel } from "discord.js";
+import type { Snowflake } from "discord-api-types";
+import type { CommandInteractionOption, TextChannel } from "discord.js";
+import { log } from "../logger";
+import moderationActionLogModel from "../models/moderationActionLog.model";
 import { getState } from "../store/state";
 import type { Command } from "../types/command";
+import { addModerationRecordToDB } from "../util";
 
 const command: Command = {
 	name: "note",
@@ -66,10 +70,45 @@ const command: Command = {
 		(interaction.channel as TextChannel).parent?.id !== getState().config.staffCategoryId,
 	handler: async (interaction, args) => {
 		await interaction.defer({ ephemeral: command.shouldBeEphemeral(interaction) });
-		console.log(args);
-		interaction.editReply("Command is not currently supported. Testing phase.").catch(err => {
-			console.log(err);
-		});
+		const subCommandArg = args.first() as CommandInteractionOption | undefined;
+		if (subCommandArg !== undefined && subCommandArg.options !== undefined) {
+			if (subCommandArg.name === "user") {
+				//Define basic variables.
+				const userForNote = getState().client.users.cache.get(
+					subCommandArg.options.get("user")?.value as Snowflake
+				);
+				const details = subCommandArg.options.get("details")?.value as string;
+
+				//Verify user variable is satisfied.
+				if (!userForNote) {
+					interaction.editReply("Failed, there was an issue getting user for note.");
+					return;
+				}
+				//Add record, no weight.
+				addModerationRecordToDB(userForNote?.id, interaction.user.id, details, 0);
+				interaction.editReply("Note added to user successfully.").catch(err => log(err, "error"));
+			} else if (subCommandArg.name === "case") {
+				//Define basic variables.
+				const caseID = subCommandArg.options.get("caseid")?.value as number;
+				const details = subCommandArg.options.get("details")?.value as string;
+
+				//Update the, what should be 1 record, with the specified caseId.
+				moderationActionLogModel
+					.updateOne(
+						{ caseNumber: caseID },
+						{
+							note: `On ${new Date().toDateString()}, moderator <@${
+								interaction.user.id
+							}> added the note: ${details}`,
+						}
+					)
+					.catch(err => log(err, "error"));
+				interaction.editReply("Note added to the case successfully.").catch(err => log(err, "error"));
+			}
+		} else {
+			//Should Subcommand somehow fail.
+			interaction.editReply("There was an issue with the SubCommand chosen.").catch(err => log(err, "error"));
+		}
 	},
 };
 
