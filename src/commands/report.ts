807bbd0b7a923cb931a879173e5daf021c5274b4
Chris <chris.barwick16@gmail.com>
2021-10-09 18:56:49 -0700

    Update report functionality to accept multiple users

--- src/commands/report.ts
+++ src/commands/report.ts
@@ -9,74 +9,97 @@ const reportOptionReason = new SlashCommandOption(
 ).setRequired();
 
 export const reportSlashCommand = new SlashCommand("report", "Report a user to staff.");
-reportSlashCommand.addOption(reportOptionUser).addOption(reportOptionReason);
-reportSlashCommand.setCallback(async (interaction, args) => {
+reportSlashCommand.addOption(reportOptionReason).addOption(reportOptionUser);
+
+for (let i = 1; i < 9; i++) {
+	const reportOptionUser = new SlashCommandOption(`user ${i}`, `The number ${i} user to report.`, "USER");
+	reportSlashCommand.addOption(reportOptionUser);
+}
+
+reportSlashCommand.setCallback((interaction, args) => {
 	if (args.length < 2) return interaction.reply({ content: "Not enough args provided.", ephemeral: true });
 
-	await interaction.deferReply({ ephemeral: true });
+	interaction
+		.deferReply({ ephemeral: true })
+		.then(() => {
+			const client = WrappedClient.getClient();
+			const state = getState();
+			const reportChannel = client.channels.cache.get(state.config.reportChannelId);
 
-	const client = WrappedClient.getClient();
-	const state = getState();
-	const reportChannel = client.channels.cache.get(state.config.reportChannelId);
+			const reasonText = args[0].value as string;
 
-	const userObject = client.users.cache.get(args[0].value as string);
-	const reasonText = args[1].value as string;
+			const userObjects: string[] = [];
 
-	let interactionResponse = "";
+			for (let i = 1; i < args.length; i++) {
+				const userObject = client.users.cache.get(args[i].value as string);
+				if (userObject && !userObject.bot && userObject.id !== interaction.user.id) {
+					const toPush = `<@${userObject.id}>`;
+					if (!userObjects.includes(toPush)) userObjects.push(toPush);
+				}
+			}
 
-	if (!reportChannel) interactionResponse = "No reportChannel";
-	if (reportChannel && !reportChannel.isText()) interactionResponse = "Invalid reportChannel";
-	if (!userObject || userObject.bot || userObject.id === interaction.user.id)
-		interactionResponse = "Could not report that user.";
-	if (!interaction.channel?.isText()) return;
+			let interactionResponse = "";
 
-	if (interactionResponse.length > 0) {
-		return interaction.reply({ content: interactionResponse, ephemeral: true });
-	}
+			if (!reportChannel) interactionResponse = "No reportChannel";
+			if (reportChannel && !reportChannel.isText()) interactionResponse = "Invalid reportChannel";
+			if (!interaction.channel?.isText()) return;
+			if (userObjects.length === 0) interactionResponse = "No users to report.";
+			if (interactionResponse.length > 0) {
+				return interaction.editReply({ content: interactionResponse });
+			}
 
-	if (reportChannel?.isText()) {
-		await interaction.channel.send("Reporting...").then(async message => {
-			await reportChannel
-				.send({
-					embeds: [
-						new MessageEmbed({
-							fields: [
-								{
-									name: "Reporter",
-									value: `<@${interaction.user.id}>`,
-									inline: true,
-								},
-								{
-									name: "Accused",
-									value: `<@${args[0].value as string}>`,
-									inline: true,
-								},
-								{
-									name: "Jump Link",
-									value: `[Here](${message.url})`,
-									inline: true,
-								},
-								{
-									name: "Reason",
-									value: reasonText,
-								},
-							],
-							timestamp: interaction.createdTimestamp,
-							color: "#FF0000",
-						}),
-					],
-				})
-				.then(async reportMessage => {
-					message.edit("Reported.").catch(console.error.bind(console));
-					interactionResponse = "You have successfully reported the user.";
-					await Promise.all([
-						reportMessage.react("👀"),
-						reportMessage.react("✅"),
-						reportMessage.react("❌"),
-					]);
-				});
-		});
+			if (reportChannel?.isText()) {
+				interaction.channel
+					.send("Reporting...")
+					.then(async message => {
+						await reportChannel
+							.send({
+								embeds: [
+									new MessageEmbed({
+										fields: [
+											{
+												name: "Reporter",
+												value: `<@${interaction.user.id}>`,
+												inline: true,
+											},
+											{
+												name: "Accused",
+												value: userObjects.join(",\n"),
+												inline: true,
+											},
+											{
+												name: "Jump Link",
+												value: `[Here](${message.url})`,
+												inline: true,
+											},
+											{
+												name: "Reason",
+												value: reasonText,
+											},
+										],
+										timestamp: interaction.createdTimestamp,
+										color: "#FF0000",
+									}),
+								],
+							})
+							.then(async reportMessage => {
+								message.edit("Reported.").catch(console.error.bind(console));
+								interactionResponse = "You have successfully reported the user(s).";
+								interaction
+									.editReply({ content: interactionResponse })
+									.catch(console.error.bind(console));
+								await Promise.all([
+									reportMessage.react("👀"),
+									reportMessage.react("✅"),
+									reportMessage.react("❌"),
+								]);
+							});
+					})
+					.catch(console.error.bind(console));
 
-		await interaction.reply({ content: interactionResponse, ephemeral: true });
-	}
+				if (interactionResponse.length > 0)
+					interaction.editReply({ content: interactionResponse }).catch(console.error.bind(console));
+			}
+		})
+		.catch(console.error.bind(console));
 });
